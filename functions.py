54d31a640d9d39d8aba2844cee1ecf583d4d5267
Dina Khalid <69371669+dina-khalid@users.noreply.github.com>
2022-11-07 00:27:23 +0200

    Refactoring functions.py

--- functions.py
+++ functions.py
@@ -1,11 +1,19 @@
-import librosa
-import librosa.display
-import IPython.display as ipd
 import numpy as np
-import matplotlib.pyplot as plt
 
 
+#-------------------------------------- Fourier/Inverse ------------------------------------------#
 def fourier(signal):
+    """
+        Calculate fourier transform of the signal
+        Parameters
+        ----------
+        signal : array of float
+            signal points
+        Return
+        ----------
+        f : array of complex
+            fourier transform of the signal
+    """
     n_samples = len(signal)
     f_hat = np.fft.fft(signal, n_samples)
     l = (len(f_hat)-2)//2
@@ -13,72 +21,175 @@ def fourier(signal):
     return f
 
 
+def inverse_fourier(frequency_list):
+    """
+        Calculate inverse fourier transform of the signal
+        Parameters
+        ----------
+        frequency_list : array of float
+            fourier transform of the signal
+        Return
+        ----------
+        ifft : array of float
+            inverse fourier transform of the signal
+    """
+    return np.fft.ifft(frequency_list)
+
+
+
+#------------------------------------------ Time ----------------------------------------------#
 def get_time(scale, sr):
+    """
+        Calculate the time of the signal using sampling rate
+        Parameters
+        ----------
+        scale : array of float
+            magnitude of the signal at each point
+        sr : int
+            number of sampling points per second
+        Return
+        ----------
+        signal_time : array of float 
+            time of the sampled signal
+    """
     return np.arange(0, len(scale)/sr, 1/sr)
 
 
-def Instrument(f, piano_mag, guitar_mag):
 
-    # piano
-    print(piano_mag)
-    f[301211: 303045] = f[301211: 303045] * piano_mag
-    f[21211: 201211] = f[21211: 201211] * piano_mag
+#---------------------------------------- Signal Spliters -----------------------------------------#
 
-#   #guitar
-    f[8211: 21211] = f[8211: 21211] * guitar_mag
+def split_arrhythmia(ecg_freq):
+    """
+        separate arithmia components
+        Parameters
+        ----------
+        ecg_freq : array of complex
+            arrithmia and normal components 
+        
+        Return
+        ----------
+        f_arrhythmia : array of complex 
+        f_normal : array of complex 
 
-    return f
+            
+    """
+    f_arrhythmia = [0]*len(ecg_freq)
+    f_arrhythmia[150:]=ecg_freq[150:]
+    f_normal = ecg_freq[:150]
+    return f_arrhythmia, f_normal
 
 
-def play(f):
-    x = np.fft.ifft(f)
-    return x
-#   ipd.Audio(x,rate=round(sr/2))
+
+def split_music(music_freq):
+    """
+        separate music instruments
+        Parameters
+        ----------
+        music_freq : array of complex
+            array of music frequencies
+       
+        Return
+        ----------
+        f_piano : array of complex 
+        f_guitar : array of complex 
+        f_drums : array of complex 
+        f_rest : array of complex 
+
+    """
+    f_piano = [0]*len(music_freq)
+    f_guitar = [0]*len(music_freq)
+    f_drums = [0]*len(music_freq)
+
+    f_piano[301211: 303045] = music_freq[301211: 303045]
+    f_piano[21211: 81211] = music_freq[21211: 81211]
+    f_guitar [8211: 21211] =  music_freq[8211: 21211] 
+    f_drums [100: 500] = music_freq[100: 500]
+    
+    f_rest = music_freq - f_piano - f_guitar
+    return f_piano, f_guitar, f_drums, f_rest
 
 
-def inverse_fourier(frequency_list):
-    return np.fft.ifft(frequency_list)
 
-def manipulate_vowels(freq, A_factor, Y_factor, V_factor, Ch_factor, Th_factor, S_factor, O_factor, R_factor, N_factor, D_factor):
-    # A frequancies
-    freq[31500:32700] *= A_factor
-    freq[95000:96000] *= A_factor
-    freq[110000:120000] *= A_factor
 
-    # Y
-    freq[20000:29000] *= Y_factor
-    freq[36000:38000] *= Y_factor
-    freq[92000:95000] *= Y_factor
+def split_vowels(audio_freq):
+    """
+        separate audio vowels
+        Parameters
+        ----------
+        audio_freq : array of complex
+            array of Audio frequencies
+       
+        Return
+        ----------
+        f_A : Vowel A components
+        f_Y : Vowel Y components
+        f_V : Vowel V components
+        f_th : Vowel Th components
+        f_ch : Vowel Ch components
+        f_s : Vowel S components
+        f_o : Vowel O components
+        f_r : Vowel R components
+        f_n : Vowel N components
+        f_d : Vowel D components
+        f_rest : Vowel the other components
+
+    """
+
+#inizialization of components arrays
+    f_A = [0]*len(audio_freq)
+    f_Y = [0]*len(audio_freq)
+    f_V = [0]*len(audio_freq)
+    f_th = [0]*len(audio_freq)
+    f_ch = [0]*len(audio_freq)
+    f_s = [0]*len(audio_freq)
+    f_o = [0]*len(audio_freq)
+    f_r = [0]*len(audio_freq)
+    f_n = [0]*len(audio_freq)
+    f_d = [0]*len(audio_freq)
+
+
+
+    # A 
+    f_A[31500:32700] = audio_freq[31500:32700] 
+    f_A[95000:96000] = audio_freq[95000:96000]
+    f_A[110000:120000] = audio_freq[110000:120000]
+
+    # Y 
+    f_Y[20000:29000] = audio_freq[20000:29000] 
+    f_Y[36000:38000] = audio_freq[36000:38000]
+    f_Y[92000:95000] = audio_freq[92000:95000]
 
     # V
-    freq[29000:31500] *= V_factor
-    freq[96000:99000] *= V_factor
+    f_V[29000:31500] = audio_freq[29000:31500]
+    f_V[96000:99000] = audio_freq[96000:99000] 
 
     #Th
-    freq[45700:49700] *= Th_factor
+    f_th[45700:49700] = audio_freq[45700:49700]
 
     #Ch
-    freq[49700:54000] *= Ch_factor
+    f_ch[49700:54000] = audio_freq[49700:54000]
 
     #S
-    freq[56000:60000] *= S_factor
-    freq[106400:109000] *= S_factor
+    f_s[56000:60000] = audio_freq[56000:60000]
+    f_s[106400:109000] = audio_freq[106400:109000]
 
     #O
-    freq[54000:56000] *= O_factor
-    freq[100800:102000] *= O_factor
+    f_o[54000:56000] = audio_freq[54000:56000]
+    f_o[100800:102000] = audio_freq[100800:102000]
     
     #R
-    freq[33000:35500] *= R_factor
-    freq[104400:106400] *= R_factor
+    f_r[33000:35500] = audio_freq[33000:35500]
+    f_r[104400:106400] = audio_freq[104400:106400]
 
     #N
-    freq[80000:89000] *= N_factor
-    freq[103000:104400] *= N_factor
-    freq[113000:116000] *= N_factor
+    f_n[80000:89000] = audio_freq[80000:89000]
+    f_n[103000:104400] = audio_freq[103000:104400]
+    f_n[113000:116000] = audio_freq[113000:116000]
 
     #D
-    freq[116000:119000] *= D_factor
-    freq[35500:37500] *= D_factor
-    freq[43000:49000] *= D_factor
-    
+    f_d[116000:119000] = audio_freq[116000:119000]
+    f_d[35500:37500] = audio_freq[35500:37500]
+    f_d[43000:49000] = audio_freq[43000:49000]
+
+    f_rest = audio_freq - f_r - f_A - f_ch - f_d - f_n - f_Y - f_th - f_o - f_n - f_s - f_V
+    return f_A, f_Y, f_V, f_th, f_ch, f_s, f_o, f_r, f_n, f_d, f_rest
